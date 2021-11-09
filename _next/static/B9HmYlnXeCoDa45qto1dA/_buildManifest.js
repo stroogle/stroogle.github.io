03f8f49a32caf9c65f4fd3f59e3ed520ce533fbd
Colm Moore <57727110+stroogle@users.noreply.github.com>
2021-11-09 15:52:56 +1100

    First commit

---
+++ _next/static/B9HmYlnXeCoDa45qto1dA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/a9a7754c-5869e88bb3a34c58.js","static/chunks/956-5ac567f8bbe9fe21.js","static/chunks/pages/index-838fac59d0215f41.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
